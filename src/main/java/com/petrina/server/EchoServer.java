d96faa1f798e5b511d3af69a1659d2f30fe21efc
Ekaterina Petrina <kostyleva-katya@mail.ru>
2022-03-08 16:36:16 +0300

    added 6th task

--- src/main/java/com/petrina/server/EchoServer.java
+++ src/main/java/com/petrina/server/EchoServer.java
@@ -18,9 +18,64 @@ public class EchoServer {
 
   public static void main(String[] args) {
 
+    try(ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
+    ) {
 
+      while (true) {
+        System.out.println("Ожидание подключения...");
+        Socket clientSocket = serverSocket.accept();
+        System.out.println("Соединение установлено!");
+
+        in = new DataInputStream(clientSocket.getInputStream());
+        out = new DataOutputStream(clientSocket.getOutputStream());
+        serverMessage(out);
+
+
+        try {
+          while (true) {
+            String message = in.readUTF();
+
+            if (message.equals("/server-stop")) {
+              System.out.println("Сервер остановлен");
+              System.exit(0);
+            }
+
+            System.out.println("Клиент: " + message);
+            out.writeUTF(message.toUpperCase());
+
+
+          }
+
+        } catch (SocketException e) {
+          clientSocket.close();
+          System.out.println("Клиент отключился");
+        }
+      }
+
+    } catch (IOException e) {
+      e.printStackTrace();
+
+    }
   }
 
+  private static void serverMessage(DataOutputStream out) {
+    Thread t = new Thread(() -> {
+      try {
+        while (true) {
+          if (out.size() == 0) {
+            String message = scanner.nextLine();
+            out.writeUTF("Server: " + message);
+          }
+        }
+      } catch (IOException e) {
+        e.printStackTrace();
+        System.out.println("Ошибка");
+      }
+    });
 
+    t.setDaemon(true);
+    t.start();
+
+  }
 
 }
